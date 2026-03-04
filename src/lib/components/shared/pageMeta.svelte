5347dc485f0773918e1712caece0234b63ca8f2a
Anthony Toribio <anthonytoribio@macbookpro.myfiosgateway.com>
2026-03-03 19:02:18 -0500

    first commit

---
+++ src/lib/components/shared/pageMeta.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { page } from "$app/state";
+	// VARIABLES
+	const { title, description, ogType = "website", image = "https://svelte0.com/images/og.png" }: Props = $props();
+	const fullPageTitle = $derived(title ? `${title} | Svelte0` : "Svelte0");
+	const pageDescription = $derived(description || "Generate svelte projects using AI.");
+	// Props type
+	interface Props {
+		title?: string | undefined;
+		ogType?: "website" | "article";
+		description?: string;
+		image?: string;
+	}
+</script>
+
+<svelte:head>
+	<link rel="icon" href="https://svelte0.com/images/favicon.png" />
+	<!-- Primary Meta Tags -->
+	<title>{fullPageTitle}</title>
+	<meta name="title" content={fullPageTitle} />
+	<meta name="description" content={pageDescription} />
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content={ogType} />
+	<meta property="og:url" content={page.url.href} />
+	<meta property="og:title" content={fullPageTitle} />
+	<meta property="og:description" content={pageDescription} />
+	<meta property="og:image" content={image} />
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={page.url.href} />
+	<meta property="twitter:title" content={fullPageTitle} />
+	<meta property="twitter:description" content={pageDescription} />
+	<meta property="twitter:image" content={image} />
+</svelte:head>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import PageMeta from "$lib/components/shared/pageMeta.svelte";
+</script>
+
+<PageMeta />
+<h1>Welcome to Svelte0</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit" class="text-orange-500">svelte0</a> for more</p>
